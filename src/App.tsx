d337ddac7478dd2f88e74916cdb55af7211a86cd
Igor Santos <falaigors@gmail.com>
2022-08-04 00:06:15 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <div></div>
+    </ThemeProvider>
+  );
+}
